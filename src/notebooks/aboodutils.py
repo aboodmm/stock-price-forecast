4e252da06e48c90317ee9011bf705c8c2d7572ba
Abdurrahman <aboodmm@gmail.com>
2026-03-16 15:27:53 -0700

    add legends, labels to plots and show test data against forecasted data

--- src/notebooks/aboodutils.py
+++ src/notebooks/aboodutils.py
@@ -1,7 +1,15 @@
 from pandas import DataFrame
 
-def truncate_date(df: DataFrame, datestring: str, datecol: str) -> None:
+def truncate_date(df: DataFrame, datestring: str, datecol: str, ahead: bool) -> None:
 
+    # Take input date string and locate matching row
     row = df.loc[df[datecol] == datestring]
 
-    print(row.index)
+    # Slice df ahead of given date, or before given date.
+    if ahead:
+        df = df.loc[row.index[0]:]
+    else:
+        df = df.loc[:row.index[0]]
+
+    return df
+
